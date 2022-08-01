5893209277b380f684abeebafc6f0f4fdd2aca33
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2022-08-01 14:59:41 +0600

    Implement update recipe tags feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -237,7 +237,7 @@ class PrivateRecipeAPITests(TestCase):
 
         payload = {'tags': []}
         url = detail_url(recipe.id)
-        res = self.client.delete(url, payload, format='json')
+        res = self.client.patch(url, payload, format='json')
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.tags.count(), 0)
