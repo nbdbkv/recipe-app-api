cc904b6ec828fb9aa21116942c456006dd8aeef0
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2022-08-02 21:39:40 +0600

    Modify recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,18 @@
+import uuid
+import os
+
 from django.conf import settings
 
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin,
 )
+
+
+def recipe_image_file_path(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -43,6 +52,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
